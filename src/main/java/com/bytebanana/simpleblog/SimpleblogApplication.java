60c4ef8f12a212ec21202a1736f5f6a47c2bdd6c
Saharat Paynok <saharat.paynok@gmail.com>
2021-01-03 20:02:09 +0700

    Init spring project

---
+++ src/main/java/com/bytebanana/simpleblog/SimpleblogApplication.java
@@ -0,0 +1,13 @@
+package com.bytebanana.simpleblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleblogApplication.class, args);
+	}
+
+}

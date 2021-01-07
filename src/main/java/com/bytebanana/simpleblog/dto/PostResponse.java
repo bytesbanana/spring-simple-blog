0f7cb7f3c66bb959cb1f4a1c2cde647d866eb461
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2021-01-07 22:42:41 +0700

    Add post response and swagger ui

---
+++ src/main/java/com/bytebanana/simpleblog/dto/PostResponse.java
@@ -0,0 +1,19 @@
+package com.bytebanana.simpleblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostResponse {
+    private Long postId;
+    private String title;
+    private String content;
+    private Instant createDate;
+    private Instant lastUpdateDate;
+    private Long userId;
+}
